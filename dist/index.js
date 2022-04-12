60abde3af25522bdd812c1940892f19a6de70c29
Carmen Fan <wind5320@hotmail.com>
2022-04-12 17:23:55 +0100

    more debugging

--- dist/index.js
+++ dist/index.js
@@ -8471,20 +8471,39 @@ var __webpack_exports__ = {};
 const core = __nccwpck_require__(2186);
 const github = __nccwpck_require__(5438);
 
-// let octokit;
+let octokit;
 
 const extractInputs = () => {
 	const pr = parseInt(core.getInput('pr'), 10);
 
-	/* const token = core.getInput('github-token');
+	const token = core.getInput('github-token');
 	octokit = github.getOctokit(token);
-	*/
+
 	return { pr };
+};
+
+const getPR = () => async (prNum) => {
+	try {
+		const payload = {
+			owner: github.context.payload.repository.owner.name,
+			repo: github.context.payload.repository.name,
+			pull_number: prNum,
+
+		};
+		const [isMerged, prData] = await Promise.all([
+			octokit.rest.pulls.checkIfMerged(payload),
+			octokit.rest.pulls.get(payload),
+		]);
+
+		console.log(isMerged, prData.base, prData.head);
+	} catch ({ message }) {
+		throw new Error(`Failed to find PR: ${message}`);
+	}
 };
 
 const run = async () => {
 	const { pr } = extractInputs();
-	console.log(pr);
+	const prData = await getPR(pr);
 };
 run().catch((err) => {
 	core.setFailed(err.message);
--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-// let octokit;
+let octokit;
 
 const extractInputs = () => {
 	const pr = parseInt(core.getInput('pr'), 10);
 
-	/* const token = core.getInput('github-token');
+	const token = core.getInput('github-token');
 	octokit = github.getOctokit(token);
-	*/
+
 	return { pr };
+};
+
+const getPR = () => async (prNum) => {
+	try {
+		const payload = {
+			owner: github.context.payload.repository.owner.name,
+			repo: github.context.payload.repository.name,
+			pull_number: prNum,
+
+		};
+		const [isMerged, prData] = await Promise.all([
+			octokit.rest.pulls.checkIfMerged(payload),
+			octokit.rest.pulls.get(payload),
+		]);
+
+		console.log(isMerged, prData.base, prData.head);
+	} catch ({ message }) {
+		throw new Error(`Failed to find PR: ${message}`);
+	}
 };
 
 const run = async () => {
 	const { pr } = extractInputs();
-	console.log(pr);
+	const prData = await getPR(pr);
 };
 run().catch((err) => {
 	core.setFailed(err.message);
