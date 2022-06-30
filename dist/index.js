43adf1337e4b6db48026f275cd41870959079bf7
Carmen Fan <wind5320@hotmail.com>
2022-06-30 17:02:34 +0100

    make checkIfMerge to return a boolean

--- dist/index.js
+++ dist/index.js
@@ -8496,7 +8496,7 @@ const getPR = async (prNum) => {
 
 		console.log('!!!PR num is ', prNum);
 		const content = await Promise.all([
-			octokit.rest.pulls.checkIfMerged(payload),
+			octokit.rest.pulls.checkIfMerged(payload).then(() => true).catch(() => false),
 			octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}?state=all', payload),
 		]);
 		console.log('returning', content);
