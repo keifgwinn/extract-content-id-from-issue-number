660e2d9fd107eadad832ed4f83fb5fa647dc406d
Carmen Fan <wind5320@hotmail.com>
2022-06-30 15:51:46 +0100

    try pull list

--- dist/index.js
+++ dist/index.js
@@ -8496,7 +8496,8 @@ const getPR = async (prNum) => {
 		console.log('!!!PR num is ', prNum);
 		const content = await Promise.all([
 			//			octokit.rest.pulls.checkIfMerged(payload),
-			octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}?state=all', payload),
+			//			octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}?state=all', payload),
+			octokit.rest.pulls.list(payload),
 		]);
 		console.log('returning', content);
 		return content;
