d7d0b2b2f1cabddf2acf631954f3d01d1de705a8
Keif Gwinn <keif.gwinn@3drepo.org>
2022-11-04 12:50:07 +0000

    differnt ID needed for project?

--- index.js
+++ index.js
@@ -42,10 +42,10 @@ const run = async () => {
 	const [issueData] = await getIssue(issueNumber);
 	console.log('"debugging issue data"')
 	console.log(issueData)
-	if (issueData.data.node_id) {
-		core.setOutput('content-id', issueData.data.node_id);
+	if (issueData.data.id) {
+		core.setOutput('content-id', issueData.data.id);
 	} else {
-		console.log(`${!issueData.node_id ? '' : 'help?'}. No action needed`);
+		console.log(`${!issueData.id ? '' : 'help?'}. No action needed`);
 	}
 };
 run().catch((err) => {
